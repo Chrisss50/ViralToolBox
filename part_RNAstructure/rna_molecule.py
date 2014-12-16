eb7e6e3794642d4d12d739fb28f28c4f59147566
sven1103 <sven.fillinger@gmail.com>
2014-12-16 14:03:06 +0100

    update: bulding ct-file parser

--- part_RNAstructure/rna_molecule.py
+++ part_RNAstructure/rna_molecule.py
@@ -71,18 +71,48 @@ class RNA_molecule:
 
 def parse_database(path_db):
     db_name = "rna.struc"
-    path_db += db_name
-    print path_db
+    path_db_file = path_db + db_name
+    print path_db_file
 
-    handler = open(path_db, "w")
-    print "Worked..."
-    return handler
+    handler = open(path_db_file, "w")
+
+    # read all ct-file in the directory of the database path
+    for file in os.listdir(path_db):
+        # check if file is a ct-file
+        if file.endswith(".ct"):
+            ct_file = open(path_db+file, "r")
+            seq_length = 0
+            newick_str = None
+            sequence_str = None
+            # go through every line of the file
+            for line in ct_file:
+                # skip the comment lines
+                if line[0] is not "#":
+                    print file
+                    print line
+                    print seq_length
+                    if seq_length is 0:
+                        # get the sequence length number
+                        seq_length = int(filter(None, (line.split(" ")))[0])
+                        newick_str = [None] * seq_length
+                    else:
+                        # make list from to extract the bracket and dots
+                        # positions
+                        line = filter(None, line.split(" "))
+                        if newick_str[int(line[2])] is None:
+                            # when a dot notation should be done
+                            if int(line[4]) is 0:
+                                newick_str[int(line[2])] = "."
+                            else:
+                                # make the opening and closing brackets
+                                newick_str[int(line[2])] = "("
+                                newick_str[int(line[4])] = ")"
+        print "".join(newick_str)
+    return path_db_file
 
 
-def main():
+if __name__ == "__main__":
     mol = RNA_molecule("CATGC", "HI-V", "test")
     mol.print_rna_information()
-    mol.db_parsed("RNA_STRAND_daa/")
+    mol.db_parsed("RNA_STRAND_data/")
     mol.print_rna_information()
-
-main()

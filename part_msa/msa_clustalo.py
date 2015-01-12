148a88ad92a354c7db7c6505ea733c6f38fc8fcb
Julian Regalado <julian.regalado@tuebingen.mpg.de>
2015-01-12 13:32:40 +0100

    Logging added module complete

--- part_msa/msa_clustalo.py
+++ part_msa/msa_clustalo.py
@@ -1,18 +1,21 @@
 #! /usr/bin/python
 import sys
 import msa_functions
+import os
 
 args = sys.argv
 
 def main():
-    msa_functions.checkargs(args)
+    r, err = os.pipe()
+    err = os.fdopen(err, 'w')
+    msa_functions.checkargs(args,err)
     fastafile = args[1]
     outfile = args[2]
     print "Infile is",fastafile,"\nOutfile is",outfile
-    msa_functions.checkclustal()
-    msa_functions.checkfasta(fastafile)
+    msa_functions.checkclustal(err)
+    msa_functions.checkfasta(fastafile,err)
     print "Starting clustalo"
-    msa_functions.runclustal(fastafile,outfile)
+    msa_functions.runclustal(fastafile,outfile,err)
 
 
 
--- part_msa/msa_functions.py
+++ part_msa/msa_functions.py
@@ -4,43 +4,43 @@ import os
 import sys
 from Bio import SeqIO as seqio
 
-def checkargs(args):
+def checkargs(args,error):
     print "Checking parameters"
     if len(args) < 3:
-        print "ERROR: Incorrect usage, not enough parameters\n" \
-              "Usage: python msa_clustalo.py <fastai input> <output file name>"
+        error.write("ERROR: Incorrect usage, not enough parameters\nUsage:\
+                    python msa_clustalo.py <fastai input> <output file name>")
         sys.exit(1)
 
     if os.path.isfile(args[1]):
         pass
     else:
-        print "ERROR:",args[1],"could not be found."
+        error.write("ERROR:",args[1],"could not be found.")
         sys.exit(1)
     if os.path.isfile(args[2]):
-        print "ERROR",args[2],"already exists."
+        error.write("ERROR",args[2],"already exists.")
         sys.exit(1)
     else:
         pass
     return None
 
-def checkclustal():
+def checkclustal(error):
     DEVNULL = open(os.devnull,'wb')
     flag = subprocess.call("which clustalo", shell=True, stdout=DEVNULL)
     if flag:
-        print "ERROR: clustalo could not be found, be shure to have it " \
-              "installed http://www.clustal.org/omega/\nor that it is i" \
-              "ncluded in your PATH"
+        error.write("ERROR: clustalo could not be found, be shure to have it\
+                    installed http://www.clustal.org/omega/\nor that it is\
+                    included in your PATH")
         sys.exit(1)
     else:
         return None
 
-def checkfasta(fastafile):
+def checkfasta(fastafile,error):
     print "Checking",fastafile,"is valid"
     seqs = seqio.parse(fastafile,'fasta')
     flag = 0
     for seq in seqs:
         if len(seq.seq) <= 0:
-            print "ERROR: Sequence",seq.id,"has 0 residues"
+            error.write("ERROR: Sequence",seq.id,"has 0 residues")
             sys.exit(1)
         for nuc in str(seq.seq):
             if nuc not in ['A','C','G','T','a','g','c','t','N']:
@@ -49,12 +49,12 @@ def checkfasta(fastafile):
                 sys.exit(1)
         flag += 1
     if not flag:
-        print "ERROR: File",fastafile,"is not a fasta file"
+        error.write("ERROR: File",fastafile,"is not a fasta file")
         sys.exit(1)
     else:
         return None
 
-def runclustal(fastafile,outfile):
+def runclustal(fastafile,outfile,error):
     print "Running: clustalo -i",fastafile,"-o",outfile
     proc = subprocess.Popen(["clustalo","-i",fastafile,"-o",outfile,
                              "--outfmt","phy","-v"],stdout=subprocess.PIPE)
